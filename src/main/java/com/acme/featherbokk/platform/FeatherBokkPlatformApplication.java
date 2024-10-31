735ebfb6f9280e665da6418303e979d8c34c9fd8
Roy Fernandez <u20221b778@upc.edu.pe>
2024-10-31 18:34:07 -0500

    feat(shared): add infrastructure layer for shared bounded context.

--- src/main/java/com/acme/featherbokk/platform/FeatherBokkPlatformApplication.java
+++ src/main/java/com/acme/featherbokk/platform/FeatherBokkPlatformApplication.java
@@ -2,6 +2,19 @@ package com.acme.featherbokk.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * FeatherBokkPlatformApplication
+ *
+ * @summary
+ * The main class of the FeatherBokk Platform application.
+ * It is responsible for starting the Spring Boot application.
+ * It also enables JPA auditing.
+ *
+ * @since 1.0
+ */
+@EnableJpaAuditing
 
 @SpringBootApplication
 public class FeatherBokkPlatformApplication {
