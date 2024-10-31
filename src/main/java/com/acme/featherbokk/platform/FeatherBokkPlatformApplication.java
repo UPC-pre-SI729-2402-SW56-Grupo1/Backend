201136c9ffe5f20f3c3111541514cee81c43de25
Roy Fernandez <u20221b778@upc.edu.pe>
2024-10-31 17:08:41 -0500

    chore: initial commit

---
+++ src/main/java/com/acme/featherbokk/platform/FeatherBokkPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.acme.featherbokk.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatherBokkPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeatherBokkPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/featherbokk/platform/FeatherBokkPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.featherbokk.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatherBokkPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
